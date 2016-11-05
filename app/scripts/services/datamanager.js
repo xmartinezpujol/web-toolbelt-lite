5f73d79ba174b7be1e2c18516592cb93d88e6c02
Xavier Martínez <xmartinezpujol@gmail.com>
2016-11-05 20:23:46 +0100

    más cambios de rutas en datamanager

--- app/scripts/services/datamanager.js
+++ app/scripts/services/datamanager.js
@@ -14,7 +14,7 @@ angular.module('webToolbeltApp')
     };
 
     this.getCategory = function(id){
-      var categoryFile = '/' + 'category/' + id + '.json';
+      var categoryFile = '../' + 'category/' + id + '.json';
       console.log(categoryFile);
       return $.get(categoryFile);
     };
